b5a2dc2d6f40f3dd10028d45aa54519380ddab44
sharon884 <sharonsharon9048461583@gmail.com>
2026-09-06 14:55:59 +0530

    feat(frontend): add responsive navigation

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,6 +1,10 @@
+import Navbar from "../components/layout/Navbar";
+
 function LandingPage() {
     return (
         <main>
+            <Navbar />
+
             <h1>Reach</h1>
         </main>
     );
