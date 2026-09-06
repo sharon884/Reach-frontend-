aa3569e35023a96e3e37ea50c3add977d99b6138
sharon884 <sharonsharon9048461583@gmail.com>
2026-09-06 15:23:44 +0530

    feat(landing): complete landing page

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -3,6 +3,7 @@ import HeroSection from "../components/landing/ HeroSection";
 import CommunityFeed from "../components/landing/CommunityFeed";
 import LifecycleSection from "../components/landing/LifecycleSection";
 import CTASection from "../components/landing/CTASection";
+import Footer from "../components/landing/Footer";
 
 function LandingPage() {
     return (
@@ -12,6 +13,7 @@ function LandingPage() {
             <CommunityFeed />
             <LifecycleSection />
             <CTASection/>
+            <Footer/>
         </main>
     );
 }
