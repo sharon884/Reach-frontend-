ccbbdec46c75841977a6644f1bc1bb79637be80a
sharon884 <sharonsharon9048461583@gmail.com>
2026-09-06 14:28:42 +0530

    chore(frontend): initialize react application

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+    return (
+        <div className="flex min-h-screen items-center justify-center">
+            <h1 className="text-4xl font-bold">
+                Reach
+            </h1>
+        </div>
+    );
+}
+
+export default App;
