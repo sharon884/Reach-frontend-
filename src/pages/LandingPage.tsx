2f28195756576b661cac5a306fb38b9d987eb63c
sharon884 <sharonsharon9048461583@gmail.com>
2026-09-10 15:30:28 +0530

    refactor(frontend): move hero section to organisms

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,5 +1,5 @@
 import Navbar from "../components/organisms/Navbar";
-import HeroSection from "../components/landing/ HeroSection";
+import HeroSection from "../components/organisms/HeroSection";
 import CommunityFeed from "../components/landing/CommunityFeed";
 import LifecycleSection from "../components/landing/LifecycleSection";
 import CTASection from "../components/landing/CTASection";
