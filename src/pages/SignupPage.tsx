c68b0010506efaea36f51bdd73fb135b1b6fc4e3
sharon884 <sharonsharon9048461583@gmail.com>
2026-09-18 11:30:51 +0530

    refactor: modularize password requirements component

--- src/pages/SignupPage.tsx
+++ src/pages/SignupPage.tsx
@@ -9,7 +9,7 @@ import { signup, googleLogin } from "@/services/auth/auth.service";
 import Button from "@/components/atoms/Button/Button";
 import FormField from "@/components/molecules/FormField/FormField";
 import PasswordField from "@/components/molecules/PasswordField/PasswordField";
-import PasswordRequirements from "@/components/molecules/PasswordRequirements";
+import PasswordRequirements from "@/components/molecules/PasswordRequirements/PasswordRequirements";
 import GoogleSignInButton from "@/components/molecules/GoogleSignInButton/GoogleSignInButton";
 
 import { signupSchema } from "@/schemas/auth/signup.schema";
